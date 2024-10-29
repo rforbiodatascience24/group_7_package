d7cb47f47b6061f6bacaa91306a67fe3e6237162
Sjofski <asjokumsen@gmail.com>
2024-10-29 10:02:48 +0100

    created histogram of occurrences function

--- R/Function_five.R
+++ R/Function_five.R
@@ -1,1 +1,21 @@
-min_version = TRUE
+plot_occurrence <- function(amino_acid_sequence){
+  unique_amino_acids <- amino_acid_sequence |>
+    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
+    as.character() |>
+    unique()
+
+  counts <- sapply(unique_amino_acids, function(amino_acid) stringr::str_count(string = amino_acid_sequence, pattern =  amino_acid)) |>
+    as.data.frame()
+
+  colnames(counts) <- c("Counts")
+  counts[["amino_acid_sequence"]] <- rownames(counts)
+
+  amino_acid_occurrence <- counts |>
+    ggplot2::ggplot(ggplot2::aes(x = Name_me2, y = Counts, fill = Name_me2)) +
+    ggplot2::geom_col() +
+    ggplot2::theme_bw() +
+    ggplot2::theme(legend.position = "none")
+
+  return(amino_acid_occurrence)
+}
+
