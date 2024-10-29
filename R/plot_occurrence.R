5729f3b0fe02806aa7545cfc029c7d22cb6f470e
Sjofski <asjokumsen@gmail.com>
2024-10-29 11:21:58 +0100

    Added description for function 5

--- R/plot_occurrence.R
+++ R/plot_occurrence.R
@@ -4,29 +4,40 @@
 #'
 #' @param amino_acid_sequence A character string containing a sequence of amino acids.
 #'
-#' @return A histogram plot of the occurence of each unique amino acid.
+#' @return A barplot of the occurence of each unique amino acid.
 #' @export
 #'
-#' @examples sequence <- "ACDEFGHIKLMNPQRSTVWYACDEFGHIKLMN"
+#' @examples sequence <- paste0(sample(c("A", "C", "D", "E", "F", "G", "H",
+#'                                       "I", "K", "L", "M", "N", "P", "Q",
+#'                                       "R", "S", "T", "V", "W", "Y"),
+#'                                       1000, replace = TRUE), collapse = "")
+#'
 #' plot_occurrence(sequence)
 #'
 plot_occurrence <- function(amino_acid_sequence){
   unique_amino_acids <- amino_acid_sequence |>
-    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
+    stringr::str_split(pattern = stringr::boundary("character"),
+                       simplify = TRUE) |>
     as.character() |>
     unique()
 
-  counts <- sapply(unique_amino_acids, function(amino_acid) stringr::str_count(string = amino_acid_sequence, pattern =  amino_acid)) |>
+  counts <- sapply(unique_amino_acids,
+                   function(amino_acid)
+                     stringr::str_count(string = amino_acid_sequence,
+                                        pattern =  amino_acid)) |>
     as.data.frame()
 
   colnames(counts) <- c("Counts")
   counts[["amino_acid_sequence"]] <- rownames(counts)
 
   amino_acid_occurrence <- counts |>
-    ggplot2::ggplot(ggplot2::aes(x = amino_acid_sequence, y = Counts, fill = amino_acid_sequence)) +
+    ggplot2::ggplot(ggplot2::aes(x = amino_acid_sequence,
+                                 y = Counts,
+                                 fill = amino_acid_sequence)) +
     ggplot2::geom_col() +
     ggplot2::theme_bw() +
-    ggplot2::theme(legend.position = "none")
+    ggplot2::theme(legend.position = "none",
+                   axis.title.x = "Amino acid")
 
   return(amino_acid_occurrence)
 }
