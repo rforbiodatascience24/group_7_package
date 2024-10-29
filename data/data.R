fc6d4d64211b45dbc97cb9374df2555f739d4c83
Sjofski <asjokumsen@gmail.com>
2024-10-29 09:09:28 +0100

    Added the central codon

---
+++ data/data.R
@@ -0,0 +1,8 @@
+#' The standard codon table of the central dogma
+#'
+#' The codon table describing the conversion of base pair triplets to amino acids during
+#' trnslation of the central dogma.
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
