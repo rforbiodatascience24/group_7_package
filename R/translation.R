82445402353bc883ff4b88e12c095ef4aaa538d2
bbd52 <s233415@dtu.dk>
2024-10-31 15:47:30 +0100

    Update function 4: translation.R for consistency
    replaced rna_seq with RNA, to keep consistency with the transcription.R, which defines the created sequence of RNA as "RNA"

--- R/translation.R
+++ R/translation.R
@@ -2,13 +2,13 @@
 #'
 #' This function translates an RNA sequence into an amino acid chain using a codon table.
 #'
-#' @param rna_seq A character string representing the RNA sequence.
+#' @param RNA A character string representing the RNA sequence.
 #' @return A character string of the amino acid sequence.
 #' @examples
 #' translate_rna("AUGGCCAUGGCGCCCAGAACUG")
 #' @export
-translate_rna <- function(rna_seq) {
-  codons <- strsplit(rna_seq, "(?<=.{3})", perl = TRUE)[[1]]
+translate_rna <- function(RNA) {
+  codons <- strsplit(RNA, "(?<=.{3})", perl = TRUE)[[1]]
   # Check for valid codons and replace invalid ones with a warning or empty string
   aa_seq <- paste(sapply(codons, function(codon) {
     if (!is.null(codon_table[codon])) codon_table[codon] else {
