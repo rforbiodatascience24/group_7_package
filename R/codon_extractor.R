99606e9a5bdc794e25efbf7c87cae43f142dc70c
Soph-DTU <s225099L@dtu.dk>
2024-10-31 18:57:18 +0100

    Committing quick edit

--- R/codon_extractor.R
+++ R/codon_extractor.R
@@ -1,13 +1,14 @@
 #' Codon_extractor
 #'
 #' @param input_sequence a string containing a DNA/RNA sequence
+#' @param start a integer indicating starting position for codon extraction (default is one)
 #'
-#' @return a string of codons constructed from input sequence
+#' @return a string of codons (triplets) constructed from input sequence
 #' @export
 #'
 #' @examples
 #' # codon_extractor("ATTGATTTA")
-#' # output: "ATT GAT TTA"
+#' # output: c("ATT","GAT","TTA")
 #'
 codon_extractor <- function(input_sequence, start = 1){
   sequence_length <- nchar(input_sequence)
